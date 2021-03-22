3809f133d0b665ec4169a7455df7518bfd8cd8fe
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-03-22 10:19:03 +0300

    Добавлена главная страница.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-from flask import jsonify
+from flask import jsonify, render_template
+
 from app import app
 
 data = ["task1", "task2", "task3", "task4", "task5", "task6", "task7", "task8"]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 @app.route('/tasks', methods=['GET'])
--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-import logging
-
 from app import app
-
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
 
 if __name__ == '__main__':
     app.run()
